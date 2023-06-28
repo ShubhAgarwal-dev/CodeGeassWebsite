2ba29aa84e7f5d2a17cefce9436d726fc086365d
Shubh Agarwal <shubhagarwa8888@gmail.com>
2023-06-28 11:28:49 +0530

    some quality of life improvements

--- components/Registration/RegBlock/RegBlock.tsx
+++ components/Registration/RegBlock/RegBlock.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react'
 import Loading from '@/components/Loading/Loading'
 import useAuth from '@/hooks/useAuth'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 import styles from './RegBlock.module.css'
 
@@ -19,7 +19,7 @@ interface Props {
 
 const RegBlock = ({ title, inputs, handleChangeInput, type }: Props) => {
   const [authState, setAuthState] = useState<State>({
-    loading: true,
+    loading: false,
     data: null,
     error: null,
   })
@@ -30,7 +30,7 @@ const RegBlock = ({ title, inputs, handleChangeInput, type }: Props) => {
   useEffect(() => {
     if (
       inputs.fullName &&
-      String(inputs.rollNumber).length > 9 &&
+      String(inputs.rollNumber).length >= 9 &&
       inputs.userHandle
     ) {
       return setDisabled(false)
@@ -54,109 +54,113 @@ const RegBlock = ({ title, inputs, handleChangeInput, type }: Props) => {
       },
       setAuthState,
     )
-    if (authState.data) {
+    if (authState.error === null) {
       router.push('/leaderboard')
     }
   }
 
   return (
     <>
-      {authState.loading ? <Loading /> : null}
       <div className={styles.FormHeading}>
         <div className={styles.text_block}>
           <h2>{title}</h2>
         </div>
       </div>
-      <div className={`${styles.formWrapper}`}>
-        {authState.error ? (
-          <div
-            className='p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400'
-            role='alert'
-          >
-            <span className='font-medium'>Danger alert!</span> {authState.error}
-          </div>
-        ) : null}
-        <form className='w-full max-w-sm'>
-          <div className='md:flex md:items-center mb-6'>
-            <div className='md:w-1/3 content-center'>
-              <label
-                className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
-                htmlFor='inline-full-name'
-              >
-                Full Name
-              </label>
+      {authState.loading ? (
+        <Loading />
+      ) : (
+        <div className={`${styles.formWrapper}`}>
+          {authState.error ? (
+            <div
+              className='p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400'
+              role='alert'
+            >
+              <span className='font-medium'>Danger alert!</span>{' '}
+              {authState.error}
             </div>
-            <div className='md:w-2/3'>
-              <input
-                className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-900'
-                id='inline-full-name'
-                type='text'
-                value={inputs.fullName}
-                onChange={handleChangeInput}
-                name='fullName'
-                required
-              />
+          ) : null}
+          <form className='w-full max-w-sm'>
+            <div className='md:flex md:items-center mb-6'>
+              <div className='md:w-1/3 content-center'>
+                <label
+                  className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+                  htmlFor='inline-full-name'
+                >
+                  Full Name
+                </label>
+              </div>
+              <div className='md:w-2/3'>
+                <input
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-900'
+                  id='inline-full-name'
+                  type='text'
+                  value={inputs.fullName}
+                  onChange={handleChangeInput}
+                  name='fullName'
+                  required
+                />
+              </div>
             </div>
-          </div>
-          <div className='md:flex md:items-center mb-6'>
-            <div className='md:w-1/3'>
-              <label
-                className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
-                htmlFor='inline-full-name'
-              >
-                Roll Number
-              </label>
+            <div className='md:flex md:items-center mb-6'>
+              <div className='md:w-1/3'>
+                <label
+                  className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+                  htmlFor='inline-full-name'
+                >
+                  Roll Number
+                </label>
+              </div>
+              <div className='md:w-2/3'>
+                <input
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-900'
+                  id='inline-full-name'
+                  type='text'
+                  value={inputs.rollNumber}
+                  onChange={handleChangeInput}
+                  name='rollNumber'
+                  required
+                  minLength={9}
+                  maxLength={9}
+                />
+              </div>
             </div>
-            <div className='md:w-2/3'>
-              <input
-                className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-900'
-                id='inline-full-name'
-                type='text'
-                value={inputs.rollNumber}
-                onChange={handleChangeInput}
-                name='rollNumber'
-                required
-                minLength={9}
-                maxLength={9}
-              />
+            <div className='md:flex md:items-center mb-6'>
+              <div className='md:w-1/3'>
+                <label
+                  className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+                  htmlFor='inline-full-name'
+                >
+                  User Handle
+                </label>
+              </div>
+              <div className='md:w-2/3'>
+                <input
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-900'
+                  id='inline-full-name'
+                  type='text'
+                  value={inputs.userHandle}
+                  onChange={handleChangeInput}
+                  name='userHandle'
+                  required
+                />
+              </div>
             </div>
-          </div>
-          <div className='md:flex md:items-center mb-6'>
-            <div className='md:w-1/3'>
-              <label
-                className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
-                htmlFor='inline-full-name'
-              >
-                User Handle
-              </label>
+            <div className='md:flex md:items-center'>
+              <div className='md:w-1/3'></div>
+              <div className='md:w-2/3'>
+                <button
+                  className='shadow bg-gray-700 hover:bg-gray-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
+                  type='button'
+                  disabled={disabled}
+                  onClick={handleSubmit}
+                >
+                  Register
+                </button>
+              </div>
             </div>
-            <div className='md:w-2/3'>
-              <input
-                className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-900'
-                id='inline-full-name'
-                type='text'
-                value={inputs.userHandle}
-                onChange={handleChangeInput}
-                name='userHandle'
-                required
-              />
-            </div>
-          </div>
-          <div className='md:flex md:items-center'>
-            <div className='md:w-1/3'></div>
-            <div className='md:w-2/3'>
-              <button
-                className='shadow bg-gray-700 hover:bg-gray-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
-                type='button'
-                disabled={disabled}
-                onClick={handleSubmit}
-              >
-                Register
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </>
   )
 }
