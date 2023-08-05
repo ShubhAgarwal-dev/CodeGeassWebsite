06df29d6a93db263601c064c50ee485f3a562f60
Shubh Agarwal <shubhagarwa8888@gmail.com>
2023-08-06 00:13:24 +0530

    updated some project discription

--- app/projects/data.ts
+++ app/projects/data.ts
@@ -4,11 +4,22 @@ import basic_img from '@/public/Projects/Images/GRAND.png'
 
 const data: blocksData = [
   {
-    heading: 'GrandChip Replication',
+    heading: 'Student Companion App',
     content:
       'We will try to replicate GRAND Chip, or try to get near that to make a universal encoding and decoding FPGA',
-    knowMoreLink: 'https://www.granddecoder.mit.edu/',
+    knowMoreLink:
+      'https://docs.google.com/document/d/1Xg70yNGxvFGi6nF9kM9sy2WNs6MPmpqwWJGSUkJJ5DY/edit?usp=sharing',
     image: basic_img.src,
+    time: 'FOSS',
+  },
+  {
+    heading: '2D-Platformer Game',
+    content:
+      'Currently we are developing a 2D top down game ,based on the theme of ruin exploration, set on a fictional stage. This game boasts a rich world filled with exploration and adventure.',
+    knowMoreLink:
+      'https://docs.google.com/document/d/1QNEcDAQeoi_AZt7qXQF7T2RmdZCxovqfjODHy5joCzw/edit?usp=sharing',
+    image: basic_img.src,
+    time: 'Game Dev',
   },
 ]
 
