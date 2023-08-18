a301f90b91c728e6d5cf95d85dbd83cdec210ba8
Jaya Surya <pjayasurya2711@gmail.com>
2023-08-19 00:26:24 +0530

    edit events done

--- components/Block/BlockCard/BlockCardAdmin.jsx
+++ components/Block/BlockCard/BlockCardAdmin.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import styles from './BlockCard.module.css'
 import axios from 'axios'
+import Modal from 'react-modal'
 
 const BlockCardAdmin = ({ leftSideImage, data, number, getEvents }) => {
+  const [open, setOpen] = useState(false)
+  const [eventData, setEventData] = useState({
+    title: data.title,
+    description: data.description,
+    start_month: data.start_month,
+    url: data.url,
+    image_url: data.image_url,
+  })
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    setEventData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
   const handleEdit = async () => {
     try {
       const eventId = data.id
-      console.log('eventId:', eventId)
-      await axios.put(`/api/events/?eventId=${eventId}`, data)
+      await axios.put(`/api/events/?eventId=${eventId}`, eventData)
       getEvents()
+      handleClose()
     } catch (error) {
       console.error('Error editing event:', error)
     }
@@ -32,7 +58,7 @@ const BlockCardAdmin = ({ leftSideImage, data, number, getEvents }) => {
       <div className={styles.blockCardWrapper}>
         <div className={styles.blockCardMain}>
           <div>
-            <button onClick={handleEdit} style={{ backgroundColor: 'yellow' }}>
+            <button onClick={handleOpen} style={{ backgroundColor: 'yellow' }}>
               Edit
             </button>
             <button
@@ -41,6 +67,76 @@ const BlockCardAdmin = ({ leftSideImage, data, number, getEvents }) => {
             >
               Delete
             </button>
+            <Modal
+              isOpen={open}
+              onRequestClose={handleClose}
+              shouldCloseOnOverlayClick={true}
+              style={{
+                // change to separate css file
+                overlay: {
+                  zIndex: '100',
+                },
+                content: {
+                  backgroundColor: 'green',
+                  margin: 'auto',
+                  width: '70%',
+                  height: '80%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                },
+              }}
+            >
+              <h2>Edit Event</h2>
+              <input
+                type='text'
+                name='title'
+                value={eventData.title}
+                onChange={handleInputChange}
+                placeholder='Event Title'
+              />
+              <input
+                type='text'
+                name='description'
+                value={eventData.description}
+                onChange={handleInputChange}
+                placeholder='Event Description'
+              />
+              <select
+                value={eventData.start_month}
+                onChange={handleInputChange}
+                name='start_month'
+              >
+                <option value=''>Select Month</option>
+                <option value='January'>January</option>
+                <option value='February'>February</option>
+                <option value='March'>March</option>
+                <option value='April'>April</option>
+                <option value='May'>May</option>
+                <option value='June'>June</option>
+                <option value='July'>July</option>
+                <option value='August'>August</option>
+                <option value='September'>September</option>
+                <option value='October'>October</option>
+                <option value='November'>November</option>
+                <option value='December'>December</option>
+              </select>
+              <input
+                type='text'
+                name='url'
+                value={eventData.url}
+                onChange={handleInputChange}
+                placeholder='Event URL'
+              />
+              <input
+                type='text'
+                name='image_url'
+                value={eventData.image_url}
+                onChange={handleInputChange}
+                placeholder='Image URL'
+              />
+              <button onClick={handleEdit}>Edit Event</button>
+              <button onClick={handleClose}>Cancel</button>
+            </Modal>
           </div>
           <div
             className={`${styles.blockInfo} ${
