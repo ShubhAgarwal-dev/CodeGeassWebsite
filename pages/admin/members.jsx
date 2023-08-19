ff151b8d28ae1bcfa53186d47e1dbd55d95854b1
Jaya Surya <pjayasurya2711@gmail.com>
2023-08-19 15:42:33 +0530

    some things need to be fixed

--- pages/admin/members.jsx
+++ pages/admin/members.jsx
@@ -1,4 +1,68 @@
-const Members = () => {
-  return <>hello members</>
+import React, { useState } from 'react'
+
+export default function Members() {
+  const [data, setData] = useState({
+    name: '',
+    roll_number: '',
+    wing: '',
+  })
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    setData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+
+    const response = await fetch('/api/admin/members', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: data.name,
+        roll_number: data.roll_number,
+        wing: data.wing,
+      }),
+    })
+
+    if (response.ok) {
+      console.log('Member added successfully')
+    } else {
+      console.error('Error adding member')
+    }
+  }
+
+  return (
+    <div>
+      <h1>Add Member</h1>
+      <input
+        type='text'
+        name='name'
+        value={data.name}
+        onChange={handleInputChange}
+        placeholder='Name'
+      />
+      <input
+        type='text'
+        name='roll_number'
+        value={data.roll_number}
+        onChange={handleInputChange}
+        placeholder='Roll Number'
+      />
+      <input
+        type='text'
+        name='wing'
+        value={data.wing}
+        onChange={handleInputChange}
+        placeholder='Wing'
+      />
+
+      <button onClick={handleSubmit}>Add Member</button>
+    </div>
+  )
 }
-export default Members
--- pages/api/admin/members.js
+++ pages/api/admin/members.js
@@ -1,1 +1,20 @@
 import prisma from './prismaClient'
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const data = req.body
+
+      const newMember = await prisma.members.create({
+        data: data,
+      })
+
+      res.status(201).json(newMember)
+    } catch (error) {
+      console.error('Error adding member:', error)
+      res.status(500).json({ error: 'Could not add member' })
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' })
+  }
+}
